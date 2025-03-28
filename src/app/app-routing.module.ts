a3c014728a77ef709d9635a86faacd1db6baa50d
Dayana <dahianab01@gmail.com>
2025-03-27 22:19:29 -0500

    diseño pestania generos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { HistoriaComponent } from './components/historia/historia.component';
+import { GenerosComponent } from './components/generos/generos.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'principal', pathMatch:'full'},
-  {path: 'principal', component:PrincipalComponent},
-  {path: 'historia', component:HistoriaComponent}
+  {
+    path:'', 
+    redirectTo:'principal', 
+    pathMatch:'full'
+  },
+  {
+    path: 'principal', 
+    component:PrincipalComponent
+  },
+  {
+    path: 'historia', 
+    component:HistoriaComponent
+  },
+  {
+    path:'generos', 
+    component: GenerosComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { BannerNavComponent } from './components/banner-nav/banner-nav.component';
 import { HistoriaComponent } from './components/historia/historia.component';
+import { GenerosComponent } from './components/generos/generos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     BannerNavComponent,
-    HistoriaComponent
+    HistoriaComponent,
+    GenerosComponent
   ],
   imports: [
     BrowserModule,
